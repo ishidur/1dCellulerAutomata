6923e8a02b97a797f63d1597aa4d7a32ee565870
ishidu-ryota-fz <ishidu-ryota-fz@ynu.jp>
2017-04-21 15:08:32 +0900

    render cell system

--- ConsoleApplication1/1dCellularAutomata.cpp
+++ ConsoleApplication1/1dCellularAutomata.cpp
@@ -4,27 +4,73 @@
 #include "stdafx.h"
 #include <GL/glut.h>
 
-void drawSquare(double x, double y, double width, double height)
+#define CELLS_NO 51 //セルの全体数
+#define CELL_NEIGHBOR_NO 1 //セルの近傍数
+#define RULE_NO 90 //規則
+#define TIME_LIM 50 //時間
+#define STATE_NO 2 //状態数
+#define VIEW_SIZE 2.0 //画面の幅
+
+void drawSquare(double x, double y, double width, double height) //render square
 {
 	glBegin(GL_POLYGON);
 	glVertex2d(x, y);
-	glVertex2d(x+width, y);
-	glVertex2d(x+width, y+height);
-	glVertex2d(x, y+height);
+	glVertex2d(x + width, y);
+	glVertex2d(x + width, y + height);
+	glVertex2d(x, y + height);
 	glEnd();
+}
+
+void drawCell(double x, double y, int state) //render single cell
+{
+	double width = VIEW_SIZE / CELLS_NO;
+	switch (state) {
+	case 1:
+		glColor3d(1.0, 0.0, 0.0);
+		break;
+	default:
+		glColor3d(0.0, 0.0, 1.0);
+		break;
+	}
+	drawSquare(x, y, width, width);
+}
+
+void drawCellsRow(int cells[CELLS_NO], int time) //render cells row
+{
+	int i;
+	double width = VIEW_SIZE / CELLS_NO;
+	double x = -VIEW_SIZE / 2.0;
+	double y = (time - TIME_LIM / 2.0) * width;
+	for (i = 0; i < CELLS_NO; i++) {
+		drawCell(x, y, cells[i]);
+		x += width;
+	}
+}
+//TODO
+//cell update by rule
+int updateCell(int cell)
+{
+	return cell;
 }
 
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3d(1.0, 0.0, 0.0);
-	drawSquare(-1.0, -1.0, 1.0, 1.0);
-	glColor3d(0.0, 1.0, 0.0);
-	drawSquare(0.0, -1.0, 1.0, 1.0);
-	glColor3d(0.0, 0.0, 1.0);
-	drawSquare(-1.0, 0.0, 1.0, 1.0);
-	glColor3d(1.0, 1.0, 0.0);
-	drawSquare(0.0, 0.0, 1.0, 1.0);
+	int i, j, k;
+	int cells[CELLS_NO] = { 0 }; //current cells state
+	int tmpCells[CELLS_NO] = { 0 }; //temporary cells array
+	cells[(int)(CELLS_NO / 2.0)] = 1;
+	for (i = 0; i < TIME_LIM; i++) {
+		drawCellsRow(cells, i);
+		for (j = 0; j < CELLS_NO; j++)
+		{
+			tmpCells[j] = updateCell(cells[j]);
+		}
+		for (k = 0; k < CELLS_NO; k++)
+		{
+			cells[k] = tmpCells[k];
+		}
+	}
 	glFlush();
 }
 
