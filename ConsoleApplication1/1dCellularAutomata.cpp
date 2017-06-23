54c311c38df7b8c49384faa32befa74d12eca0f3
ishidu-ryota-fz <ishidu-ryota-fz@ynu.jp>
2017-06-23 09:17:34 +0900

    tmp

--- ConsoleApplication1/1dCellularAutomata.cpp
+++ ConsoleApplication1/1dCellularAutomata.cpp
@@ -4,23 +4,29 @@
 #include "stdafx.h"
 #include <GL/glut.h>
 #include <math.h>
+#include <iostream>
 
 #define CELLS_NO 51 //セルの全体数
 #define CELL_NEIGHBOR_NO 1 //セルの近傍数
-#define RULE_NO 30 //規則
-#define TIME_LIM 50 //時間
+#define RULE_NO 90 //規則
+#define TIME_LIM 51 //時間
 #define STATE_NO 2 //状態数
 #define VIEW_SIZE 2.0 //画面の幅
 
-class Rule {
+class Rule
+{
 public:
 	int ruleNo;
 	int rulePatterns;
-	Rule(int _ruleNo, int stateNo, int neighborNo) {
+
+	Rule(int _ruleNo = 0, int stateNo = 2, int neighborNo = 1)
+	{
 		ruleNo = _ruleNo;
 		rulePatterns = int(pow(double(STATE_NO), double(2) * CELL_NEIGHBOR_NO + 1));
 	}
-	int getNextState(int decimalState) {
+
+	int getNextState(int decimalState)
+	{
 		int nextState = 0;
 		int leftDecimal = ruleNo;
 		for (int i = 0; i <= decimalState; i++)
@@ -32,7 +38,7 @@ public:
 	}
 };
 
-Rule thisRule = Rule(RULE_NO, STATE_NO, CELL_NEIGHBOR_NO);
+Rule thisRule = Rule();
 
 //render square
 void drawSquare(double x, double y, double width, double height)
@@ -44,6 +50,7 @@ void drawSquare(double x, double y, double width, double height)
 	glVertex2d(x, y + height);
 	glEnd();
 }
+
 //render single cell
 void drawCell(double x, double y, int state)
 {
@@ -51,17 +58,20 @@ void drawCell(double x, double y, int state)
 	glColor3d((STATE_NO - 1 - state) / double(STATE_NO - 1), (STATE_NO - 1 - state) / double(STATE_NO - 1), (STATE_NO - 1 - state) / double(STATE_NO - 1));
 	drawSquare(x, y, width, width);
 }
+
 //render cells row
 void drawCellsRow(int cells[CELLS_NO], int time)
 {
 	double width = VIEW_SIZE / CELLS_NO;
 	double x = -VIEW_SIZE / 2.0;
-	double y = (TIME_LIM / 2.0 - time) * width;
-	for (int i = 0; i < CELLS_NO; i++) {
+	double y = (TIME_LIM / 2.0 - double(time) - 1.0) * width;
+	for (int i = 0; i < CELLS_NO; i++)
+	{
 		drawCell(x, y, cells[i]);
 		x += width;
 	}
 }
+
 //cell update by Rule
 int updateCell(int neighborCells[CELL_NEIGHBOR_NO * 2 + 1])
 {
@@ -78,14 +88,15 @@ int updateCell(int neighborCells[CELL_NEIGHBOR_NO * 2 + 1])
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	int cells[CELLS_NO] = { 0 }; //current cells state
-	int tmpCells[CELLS_NO] = { 0 }; //temporary cells array
+	int cells[CELLS_NO] = {0}; //current cells state
+	int tmpCells[CELLS_NO] = {0}; //temporary cells array
 	cells[int(CELLS_NO / 2.0)] = 1;
-	for (int i = 0; i < TIME_LIM; i++) {
+	for (int i = 0; i < TIME_LIM; i++)
+	{
 		drawCellsRow(cells, i);
 		for (int j = 0; j < CELLS_NO; j++)
 		{
-			int neighborCells[CELL_NEIGHBOR_NO * 2 + 1] = { 0 };
+			int neighborCells[CELL_NEIGHBOR_NO * 2 + 1] = {0};
 			for (int l = 0; l < CELL_NEIGHBOR_NO * 2 + 1; l++)
 			{
 				int n = (j + l - CELL_NEIGHBOR_NO) % CELLS_NO;
@@ -110,8 +121,17 @@ void init(void)
 	glClearColor(0.0, 0.0, 1.0, 1.0);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
+	int ruleNo;
+	std::cout << "rule no. up to " << pow(STATE_NO, pow(STATE_NO, CELL_NEIGHBOR_NO * 2 + 1)) << std::endl;
+	std::cin >> ruleNo;
+	while (ruleNo >= pow(STATE_NO, pow(STATE_NO, CELL_NEIGHBOR_NO * 2 + 1)) || ruleNo < 0)
+	{
+		std::cout << "out of possible rule No " << pow(STATE_NO, pow(STATE_NO, CELL_NEIGHBOR_NO * 2 + 1)) << ". type again... " << std::endl;
+		std::cin >> ruleNo;
+	}
+	thisRule = Rule(ruleNo, STATE_NO, CELL_NEIGHBOR_NO);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA);
 	glutCreateWindow(argv[0]);
